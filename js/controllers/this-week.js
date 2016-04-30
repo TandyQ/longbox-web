cc6c6dddf791225d29869129112ab21d28925bd5
Tyler Anderson <tandyq@me.com>
2016-04-30 18:13:50 -0400

    Logic for moving forward and backward by week

--- js/controllers/this-week.js
+++ js/controllers/this-week.js
@@ -7,12 +7,36 @@ myApp.controller('ThisWeekController', ['$scope', '$modal', 'Marvel', 'DateUtils
 
         $scope.$watch('weekToShow', function(newValue, oldValue) {
             $scope.isLoading = true;
-            var dateRange = DateUtils.getDateRange(new Date(newValue)); // Get first and last day of week
+            loadWeeklyComicsForDay(new Date(newValue));
+        });
+
+        $scope.moveBackOneWeek = function() {
+            $scope.isLoading = true;
+            var oneWeekAgo = new Date($scope.weekToShow);
+            oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
+            var dateString = oneWeekAgo.toUTCString();
+            dateString = dateString.split(' ').slice(0, 4).join(' ');
+            dateString = dateString.replace(',', '');
+            $scope.weekToShow = dateString;
+        };
+
+        $scope.moveForwardOneWeek = function() {
+            $scope.isLoading = true;
+            var oneWeekFromNow = new Date($scope.weekToShow);
+            oneWeekFromNow.setDate(oneWeekFromNow.getDate() + 7);
+            var dateString = oneWeekFromNow.toUTCString();
+            dateString = dateString.split(' ').slice(0, 4).join(' ');
+            dateString = dateString.replace(',', '');
+            $scope.weekToShow = dateString;
+        };
+
+        var loadWeeklyComicsForDay = function(selectedDate) {
+            var dateRange = DateUtils.getDateRange(selectedDate); // Get first and last day of week
             Marvel.getComicDataForWeek(dateRange).then(function(data) {
                 $scope.comicData = data;
                 $scope.isLoading = false;
             });
-        });
+        };
 
         $scope.openModalForComic = function(selectedComic) {
             var modalInstance = $modal.open({
