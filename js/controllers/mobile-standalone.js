6d4459210702c0ac0583c9be146e683fe19b8a01
Tyler Anderson <tandyq@me.com>
2016-04-30 09:43:50 -0400

    Code Cleanup

--- js/controllers/mobile-standalone.js
+++ js/controllers/mobile-standalone.js
@@ -1,6 +1,5 @@
 myApp.controller('MobileStandaloneController', ['$scope', function($scope) {
     $scope.shouldShowStandaloneUI = function() {
-        console.log(("standalone" in window.navigator) && window.navigator.standalone);
         return (("standalone" in window.navigator) && window.navigator.standalone);
     };
 }]);
