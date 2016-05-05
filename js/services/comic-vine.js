3caee0600f5d8d30d22e20e7632ee988c19081c3
Tyler Anderson <tandyq@me.com>
2016-05-04 21:32:11 -0400

    ComicVine API layer now loads all comics for a given week instead of just the first 100

--- js/services/comic-vine.js
+++ js/services/comic-vine.js
@@ -1,6 +1,9 @@
 myApp.factory('ComicVine', ['$rootScope', '$http', '$q', 'DateUtils', '$filter',
     function($rootScope, $http, $q, DateUtils, $filter) {
 
+        var resultsPerPage = 100;
+        var comicResults = [];
+
         var baseComicVineUrl = "http://www.comicvine.com/api/";
         var comicDateQuery = "&filter=store_date:";
 
@@ -11,6 +14,9 @@ myApp.factory('ComicVine', ['$rootScope', '$http', '$q', 'DateUtils', '$filter',
                 queryUrl += "issues/?format=jsonp&json_callback=JSON_CALLBACK";
             } else if (queryType === "series") {
                 queryUrl += "series/?format=jsonp&json_callback=JSON_CALLBACK";
+            }
+            if (options.offset) {
+                queryUrl += "&offset=" + options.offset;
             }
 
             if (options.dateRange) {
@@ -37,38 +43,52 @@ myApp.factory('ComicVine', ['$rootScope', '$http', '$q', 'DateUtils', '$filter',
         };
 
         var queryComics = function(queryUrl) {
-            console.log(queryUrl); //logging query URL
+            // console.log(queryUrl); //logging query URL
             var deferred = $q.defer();
             $http.jsonp(queryUrl).then(function successCallback(response) {
-                deferred.resolve(response.data);
+                deferred.resolve(response);
             }, function errorCallback(response) {
                 deferred.resolve(response.statusText);
             });
             return deferred.promise;
         };
 
+        var parseComics = function(comics) {
+            for (var i = 0; i < comics.results.length; i++) {
+                var comic = comics.results[i];
+                comic.series = comic.volume;
+                comic.series.name = comic.series.name + " ";
+                comic.series.resourceURI = comic.api_detail_url;
+                comic.issueNumber = comic.issue_number;
+                var imageUrl = comic.image.super_url;
+                comic.thumbnail = {
+                    extension: imageUrl.substr(imageUrl.lastIndexOf('.') + 1),
+                    path: imageUrl.substr(0, imageUrl.lastIndexOf('.'))
+                };
+                comicResults.push(comic);
+            }
+            console.log(comicResults);
+        };
+
+        var loadComicDataForWeek = function(dateRange, offset) {
+            var promise = constructURL("comic", { "dateRange": dateRange, "offset": offset }).then(queryComics).then(function(response) {
+                offset += 100;
+                var numTotalResults = response.data.number_of_total_results;
+                console.log(numTotalResults);
+                console.log(response);
+                parseComics(response.data);
+                if ((offset) > numTotalResults) {
+                    return comicResults;
+                } else {
+                    return loadComicDataForWeek(dateRange, offset);
+                }
+            });
+            return promise;
+        };
+
         var myObject = {
             getComicDataForWeek: function(dateRange) {
-                console.log(dateRange);
-                var promise = constructURL("comic", { "dateRange": dateRange }).then(queryComics).then(function(response) {
-                    relevantComics = [];
-                    for (var i = 0; i < response.results.length; i++) {
-                        var comic = response.results[i];
-                        comic.series = comic.volume;
-                        comic.series.name = comic.series.name + " ";
-                        comic.series.resourceURI = comic.api_detail_url;
-                        comic.issueNumber = comic.issue_number;
-                        var imageUrl = comic.image.super_url;
-                        comic.thumbnail = {
-                            extension: imageUrl.substr(imageUrl.lastIndexOf('.')+1),
-                            path: imageUrl.substr(0, imageUrl.lastIndexOf('.'))
-                        };
-                        relevantComics.push(comic);
-                    }
-                    console.log(relevantComics);
-                    return relevantComics;
-                });
-                return promise;
+                return loadComicDataForWeek(dateRange, 0);
             }
         };
 
