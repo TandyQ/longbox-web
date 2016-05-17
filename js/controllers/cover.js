b27ae0c70d3fd05593f1b9c6d111cb459a2eb38c
Tyler Anderson <tandyq@me.com>
2016-05-17 15:29:48 -0400

    Prevent taps from going through unless the cover overlay is actually present

--- js/controllers/cover.js
+++ js/controllers/cover.js
@@ -1,10 +1,10 @@
 myApp.controller('CoverController', ['$scope', function($scope) {
-    $scope.shouldShowOverlay = false;
+    $scope.showingOverlay = false;
     $scope.tapOverlayCSS = {};
     $scope.toggleOverlay = function toggleOverlay(event) {
         if (Modernizr.touch && (event.target.nodeName.toLowerCase() !== "button") && (event.target.nodeName.toLowerCase() !== "i")) {
-            $scope.shouldShowOverlay = !$scope.shouldShowOverlay;
-            if ($scope.shouldShowOverlay) {
+            $scope.showingOverlay = !$scope.showingOverlay;
+            if ($scope.showingOverlay) {
                 $scope.tapOverlayCSS = {
                     "visibility": "visible",
                     "opacity": "1"
@@ -15,6 +15,10 @@ myApp.controller('CoverController', ['$scope', function($scope) {
                     "opacity": "0"
                 };
             }
+        } else if (Modernizr.touch &&
+            ((event.target.nodeName.toLowerCase() == "button") || (event.target.nodeName.toLowerCase() == "i")) &&
+            !$scope.showingOverlay) {
+            // do nothing
         }
     };
 }]);
